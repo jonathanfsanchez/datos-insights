f7c7fd37c519382c2ca7646d1bf5ffd7379cb85c
Jonathan Sanchez <sanchez.jonathan@gmail.com>
2019-06-12 23:25:27 -0400

    Reviews can be deleted
    must update to check ownership - uploading for mobile work

--- datos/reviews/views.py
+++ datos/reviews/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView
 
 from datasets.models import Dataset
 from models.models import Model
@@ -34,6 +36,11 @@ def review_dataset_update(request, pk, template_name='reviews/review_dataset_for
     return render(request=request, template_name=template_name, context={'form': form})
 
 
+# class DatasetReviewDelete(DeleteView):
+#     model = DatasetReview
+#     success_url = reverse_lazy('profile_view')
+
+
 def review_model_view(request, pk, template_name='reviews/review_model_view.html'):
     model_review = get_object_or_404(ModelReview, pk=pk)
     return render(request=request, template_name=template_name, context={'model_review': model_review})
@@ -58,6 +65,13 @@ def review_model_update(request, pk, template_name='reviews/review_model_form.ht
         form.save()
         return redirect('models:model_view', pk=model_review.model.pk)
     return render(request=request, template_name=template_name, context={'form': form})
+
+
+class ModelReviewDelete(DeleteView):
+    template_name = 'reviews/review_model_confirm_del.html'
+
+    model = ModelReview
+    success_url = reverse_lazy('subscriptions:subscription_list_view')
 
 # def dataset_delete(request, pk, template_name='datasets/dataset_confirm_delete.html'):
 #     dataset = get_object_or_404(Dataset, pk=pk)
