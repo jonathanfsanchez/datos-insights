ec149ced79419961c905e1a40267bd505dc7378a
Jonathan Sanchez <sanchez.jonathan@gmail.com>
2019-07-02 05:01:25 +0000

    added user fields/change forms and expanded profile view

--- datos/users/admin.py
+++ datos/users/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
+from .forms import DatosUserCreationForm, DatosUserChangeForm
 from .models import DatosUser
 
-# Register your models here.
-admin.site.register(DatosUser, UserAdmin)
+
+# custom user admin creation for profile views
+class CustomUserAdmin(UserAdmin):
+    add_form = DatosUserCreationForm
+    form = DatosUserChangeForm
+    model = DatosUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('bio',
+                           'title',
+                           'company', )}), )
+
+admin.site.register(DatosUser, CustomUserAdmin)
--- datos/users/forms.py
+++ datos/users/forms.py
@@ -1,10 +1,17 @@
 # used for overwriting django user creation
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import DatosUser
+
 
 class DatosUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = DatosUser
-        fields = ('username',)
+        fields = ('username', 'email', )
+
+
+class DatosUserChangeForm(UserChangeForm):
+
+    class Meta(UserChangeForm):
+        model = DatosUser
+        fields = ('first_name', 'last_name', 'email', 'bio', 'title', 'company', )
